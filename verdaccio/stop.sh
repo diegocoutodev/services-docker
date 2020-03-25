b686d7ac660b7c0d4b39bc1dcfa6549e1118026b
Diego do Couto <eng.coutodiego@gmail.com>
2020-03-24 22:48:36 -0300

    feat: add script to stop service

---
+++ verdaccio/stop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ ${VERDACCIO_USER_UID:-0} ]; then
+    export VERDACCIO_USER_UID=$(id -u $USER);
+fi
+
+export VERDACCIO_PROTOCOL="${VERDACCIO_PROTOCOL:-http}"
+export VERDACCIO_PORT="${VERDACCIO_PORT:-4873}";
+
+docker-compose down;
