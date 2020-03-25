7dbfe6e3793650c201f2beba5e07171d01d36bdc
Diego do Couto <eng.coutodiego@gmail.com>
2020-03-24 22:46:09 -0300

    refactor: configure enviroment variables with default values

--- verdaccio/deploy.sh
+++ verdaccio/deploy.sh
@@ -1,15 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 if [ ${VERDACCIO_USER_UID:-0} ]; then
     export VERDACCIO_USER_UID=$(id -u $USER);
 fi
 
-if [ ${VERDACCIO_PROTOCOLO:-0} ]; then
-    export VERDACCIO_PROTOCOL="http" ;
-fi
+export VERDACCIO_PROTOCOL="${VERDACCIO_PROTOCOL:-http}"
+export VERDACCIO_PORT="${VERDACCIO_PORT:-4873}";
 
-if [ ${VERDACCIO_PORT:-0} ]; then
-    export VERDACCIO_PORT=4873;
-fi
-
-docker-compose up;
+docker-compose up -d;
